40c4a106ed6c4412c0c716ae6753dc46495d2bb3
Dmitrii <shamparov@gmail.com>
2018-11-15 17:38:22 +0300

    domReady

--- js/config.js
+++ js/config.js
@@ -4,6 +4,7 @@ requirejs.config({
         "jquery": [
             "https://code.jquery.com/jquery-3.3.1.min",
             "jquery-3.3.1"],
+        "domReady": "domReady",
         "jquery-ui" : [
             "jquery-ui-1.12.1.custom/jquery-ui"
         ],
