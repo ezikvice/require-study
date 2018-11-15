62dbb0bf86e56febff03da734789f6ae64f703c5
Dmitrii <shamparov@gmail.com>
2018-11-15 14:31:37 +0300

    included jquery-ui

--- js/config.js
+++ js/config.js
@@ -5,8 +5,7 @@ requirejs.config({
             "https://code.jquery.com/jquery-3.3.1.min",
             "jquery-3.3.1"],
         "jqueryui" : [
-            "https://code.jquery.com/ui/1.12.1/jquery-ui.min",
-            "jquery-ui"
+            "jquery-ui-1.12.1.custom/jquery-ui"
         ],
     }
 });
