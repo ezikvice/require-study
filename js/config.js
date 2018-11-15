e2b9339dd719447c198c2e86ed01cb9f1e22ec9a
Dmitrii <shamparov@gmail.com>
2018-11-15 12:58:40 +0300

    first commit

---
+++ js/config.js
@@ -0,0 +1,12 @@
+requirejs.config({
+    baseUrl: "js",
+    paths: {
+        "jquery": [
+            "https://code.jquery.com/jquery-3.3.1.min",
+            "jquery-3.3.1"],
+        "jqueryui" : [
+            "https://code.jquery.com/ui/1.12.1/jquery-ui.min",
+            "jquery-ui"
+        ],
+    }
+});
